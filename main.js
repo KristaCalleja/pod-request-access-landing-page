1231c88ec828bb80d20470e916502ee6661b62af
KristaCalleja <kristacm@gmail.com>
2021-10-13 15:54:37 -0300

    Refactoring

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 const form = document.getElementById("form");
-const emailInput = document.querySelector("input[type=email]");
-// The following regexp contains an escaping for the backtick informed at column 53.
-const regex = new RegExp(`^[a-zA-Z0-9.!#$%&'*+/=?^_\`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`);
-const btn = document.getElementById("btn");
-
-function validateEmail(){
-    // Check values
-    const string = emailInput.value;
-    const checkRegex = regex.test(string); 
-    // Create new elements in form
-    const helper = document.createElement('div');
-    // Style new div
-    helper.classList.add('helper');
-    // Manipulate the DOM
-    if (checkRegex === true) {
-        helper.innerText = 'Welcome aboard! ❣';
-        form.insertAdjacentElement('afterend', helper);
-    } else {
-        helper.innerText = 'Oops! Please check your email';
-        form.insertAdjacentElement('afterend', helper);
-    }
+const liveRegion = document.querySelector(".input-aria-live");
+const testEmail = email=>{
+	let re = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+	return re.test(email);
+}
+const setErrors = (input, errorId) => {
+    input.setAttribute("aria-invalid", "true");
+    input.setAttribute("aria-describedBy", errorId);
+    input.nextElementSibling.classList.add("show-error");
+}
+const removeErrors = (input, errorId) =>{
+    input.removeAttribute("aria-invalid");
+    input.removeAttribute("aria-describedBy");
+    input.nextElementSibling.classList.remove("show-error");
+}
+const testInputs = ( input, errorId ) =>{
+	if ( input.name==="email") {
+        if ( !testEmail(input.value) ) setErrors(input, errorId);
+        else {removeErrors(input, errorId)}
+  } else {
+        if ( !input.value ) setErrors(input, errorId);
+        else {removeErrors(input, errorId)}
+  }
 }
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    validateEmail();
+    testInputs(email, "errorEmail");
 });
 // Error handler
 function handleError(err){
